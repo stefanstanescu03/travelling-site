c0ad0ee9dd0c933b7b64f872495bc56e8727b35c
Pasare Denis <denispasare11@gmail.com>
2024-02-28 14:34:00 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import { PORT } from "./config.js";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(234).send("It Works!");
+  return res.status(234).send("Merge ba!");
 });
 
 app.listen(PORT, () => {
